1675b6414042620f3c892667ec5dde185fb1777d
Emilio <emrod321@gmail.com>
2021-02-24 18:58:11 -0600

    init commit.  Layed out UI on story board

---
+++ WeatherApp/View Controllers/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Emilio Rodriguez on 2/24/21.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
