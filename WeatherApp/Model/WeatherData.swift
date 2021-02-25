62538db101266d08d6b044bab9f2f3cf51be4e6a
Emilio <emrod321@gmail.com>
2021-02-24 21:30:01 -0600

    added WeatherData.swift and WeatherModel.swift to Model

--- WeatherApp/Model/WeatherData.swift
+++ WeatherApp/Model/WeatherData.swift
@@ -1,12 +1,27 @@
-//
-//  WeatherData.swift
-//  WeatherApp
-//
-//  Created by Emilio Rodriguez on 2/24/21.
-//
 
 import Foundation
 
-struct WeatherData{
-    
+// MARK: - Result
+struct Result: Codable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp,minTemp, maxTemp: Double
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+ 
+    }
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let description: String
 }
---
+++ WeatherApp/Model/WeatherModel.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Emilio Rodriguez on 2/24/21.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let cityName: String
+    let temp: Double
+    let minTemp: Double
+    let maxTemp: Double
+    let description: String
+}
